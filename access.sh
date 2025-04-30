455dc6887d2f49bbb0bdf748161a47c54233a102
Katy <soddykathryn@gmail.com>
2025-04-29 21:19:21 -0500

    y

--- access.sh
+++ access.sh
@@ -3,3 +3,15 @@ SHOW DATABASES;
 USE `Project-Finder`;
 SHOW TABLES;
 SELECT * FROM STUDENT;
+/Users/katy/Documents/GitHub/Project-Finder/sql/STUDENT.csv
+
+
+docker volume create csv_volume
+docker run -v csv_volume:/project_finder_db/Users/katy/Documents/GitHub/Project-Finder/sql/STUDENT.csv mysql:8.0
+
+
+docker run -v csv_volume:/project_finder_db/Users/katy/Documents/GitHub/Project-Finder/sql/STUDENT.csv \
+  -e MYSQL_ROOT_PASSWORD=rooty \
+  mysql:8.0
+
+docker cp /Users/katy/Documents/GitHub/Project-Finder/sql/STUDENT.csv project_finder_db:/var/lib/mysql-files
